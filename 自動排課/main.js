f301c3c95c921fdac93b83fd61e43609086db9c6
Michelle <michelle061891@gmail.com>
2021-04-24 00:22:02 +0800

    update xx

--- 自動排課/main.js
+++ 自動排課/main.js
@@ -1,14 +1,18 @@
-$(function(){
+$(function () {
     $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
     var topicCount = topic.length;
     //一秒鐘有1000毫秒
     //每分鐘60秒、每小時60分鐘、每天24小時
-    var millisecsPerDay = 24*60*60*1000;
-    for(var x=0;x<topicCount;x++){
-    $("#courseTable").append("<tr>");
-    $("#courseTable").append(`<td>${x+1}</td>`);
-    $("#courseTable").append(`<td>${(startDate.getMonth()+7*x)}/${(startDate.getDate()+7*x)}</td>`);
-    $("#courseTable").append(`<td>${topic[x]}</td>`);
-    $("#courseTable").append("</tr>");
+    var millisecsPerDay = 24 * 60 * 60 * 1000;
+    new Date().toLocaleString(undefined, {
+        month: "numeric", day: "numeric", 
+        hour: "numeric", minute: "numeric", second: "numeric"
+    })
+    for (var x = 0; x < topicCount; x++) {
+        $("#courseTable").append("<tr>");
+        $("#courseTable").append(`<td>${x + 1}</td>`);
+        $("#courseTable").append(`<td>${(new Date(startDate.getTime() + 7 * x * millisecsPerDay)).toLocaleDateString(undefined, {month: "numeric", day: "numeric"})}</td>`);
+        $("#courseTable").append(`<td>${topic[x]}</td>`);
+        $("#courseTable").append("</tr>");
     }
-    });
+});
--- 自動排課/topic.js
+++ 自動排課/topic.js
@@ -1,23 +1,23 @@
 var topic = [
-    "尚未開學",
-    "國定假日",
-    "環境準備",
-    "隨機性",
-    "重複性",
-    "條件判斷"
-    ];
+  "尚未開學",
+  "國定假日",
+  "環境準備",
+  "隨機性",
+  "重複性",
+  "條件判斷"
+];
 var startDate = new Date();
-function setMonthAndDay(startMonth, startDay){
-//一次設定好月份與日期
-startDate.setMonth(startMonth,startDay);
-startDate.setHours(0);
-startDate.setMinutes(0);
-startDate.setSeconds(0);
+function setMonthAndDay(month, date) {
+  alert(parseInt(month, 10) + parseInt(date, 10));
+  startDate.setDate(parseInt(date, 10));
+  startDate.setMonth(parseInt(month, 10));
+  startDate.setHours(0);
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
 }
-function processFormData() {
-  const form = document.forms['form'];
-  const month = form.elements.month.value;
-  const date = form.elements.date.value;
-  alert("你的姓名是 " + month + "\n電子郵件是 " + date);
-  setMonthAndDay(4,1);
+/* function add(a, b) {
+  var sum = parseInt(a, 10) + parseInt(b, 10);
+  alert(sum);
 }
+  setMonthAndDay(month, date); 
+}*/
