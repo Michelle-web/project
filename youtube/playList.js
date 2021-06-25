a0105a00011f49606ddd071efca5d6f43b2ffbb9
Michelle <michelle061891@gmail.com>
2021-06-25 14:03:19 +0800

    update 0

---
+++ youtube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "WGgEFoI9MhE",
+    "SwZMS-ISbH4",
+    "c6gi6IP6ay4"
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [101,116],
+    [144,146],
+    [146,152]
+    ];
